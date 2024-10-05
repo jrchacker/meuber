f2f15f9f5efed694e79a9016c0d1072245464903
jrchacker <107825939+jrchacker@users.noreply.github.com>
2024-10-05 20:21:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/SEU_SCRIPT_URL/exec'; // Use a URL gerada pela Google Sheets API
+const scriptURL = '286046217337-64aq73rcil85aq3shapku4rd8tptg53n.apps.googleusercontent.com'; // Use a URL gerada pela Google Sheets API
 const form = document.getElementById('campaignForm');
 
 form.addEventListener('submit', e => {
